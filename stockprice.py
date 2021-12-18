093fea85f94aae1d9a460e6b4d06f50f7110de67
ShivangShekhar <55653469+ShivangShekhar@users.noreply.github.com>
2021-12-19 01:25:31 +0530

    Add files via upload

---
+++ stockprice.py
@@ -0,0 +1,71 @@
+import yfinance as yf
+
+import streamlit as st
+
+import pandas as pd
+
+import datetime as dt
+
+st.write("""
+
+         ## Stock Price App
+
+         Find below the stock **closing price** and the ** volume **
+
+         of the selected company
+
+          """)
+
+stocklist = {'Google':'GOOGL' ,'Amazon':'AMZN','Facebook':'FB',
+
+             'Microsoft':'MSFT','Apple':'AAPL'}
+
+ 
+
+start_date = dt.date(2010, 1,1)
+
+end_date = dt.date(2021,10,1)
+
+ 
+
+selection = st.selectbox('Pick the stock name from the dropdown',
+
+                         stocklist.keys())
+
+daterange = st.slider("Select date range", value=[start_date,end_date])
+
+date_input = st.date_input('Pick date from calendar',value=[daterange[0],daterange[1]], min_value=start_date,
+
+              max_value=end_date)
+
+ 
+
+ 
+
+tickersymbol = selection
+
+tickerdata = yf.Ticker(stocklist.get(selection))
+
+ 
+
+tickerdf = tickerdata.history(period='1d', start=str(daterange[0]),
+
+                              end=str(daterange[1]))
+
+st.write("""
+
+         ### Closing price
+
+         """)
+
+st.line_chart(tickerdf.Close)
+
+st.write("""
+
+         ### Volume
+
+         """)
+
+st.line_chart(tickerdf.Volume)
+
+ 
